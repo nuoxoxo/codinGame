25c980cf1452b3fae28f18e875ccd178e7d50e7e
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-11-14 21:35:52 +0100

    added horse race . cpp using sort

--- horse_racing_duals___Sort.cpp
+++ horse_racing_duals___Sort.cpp
@@ -7,23 +7,23 @@ using namespace std;
 
 int main()
 {
-    int n, N, i, j, d, diff = (int) 1e7;
+    int n, i, j, d, diff = (int) 1e7;
     vector<int> arr;
 
-    cin >> N; cin.ignore();
-    for (int i = 0; i < N; i++) {
+    cin >> n; cin.ignore();
+    for (int i = 0; i < n; i++) {
         int pi;
         // cout << pi << '\n';  // dbg
         cin >> pi; cin.ignore();
         arr.push_back(pi);
     }
-    if (N == 1)
+    if (n == 1)
         cout << arr[0] << endl;
     else
     {
         sort(arr.begin(), arr.end());
         i = -1;
-        while (++i < N - 1)
+        while (++i < n - 1)
         {
             d = abs(arr[i + 1] - arr[i]);
             diff = diff > d ? d : diff;
